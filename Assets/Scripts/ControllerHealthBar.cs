8da217a77f29e70da3f0ee00c15575fa60a0d5e8
GoleaF3000 <93461021+GoleaF3000@users.noreply.github.com>
2022-09-12 17:06:08 +0700

    Fixed

--- Assets/Scripts/ControllerHealthBar.cs
+++ Assets/Scripts/ControllerHealthBar.cs
@@ -6,77 +6,42 @@ using TMPro;
 
 public class ControllerHealthBar : MonoBehaviour
 {
-    [SerializeField] private TMP_Text textHealthValue;
     [SerializeField] private Slider _slider;
-    [SerializeField] private float _valueHealth;
+    [SerializeField] private ControllerHealthValue _controllerHealthValue;
     [SerializeField] private float _durationChanged;
+        
+    private float direction;    
 
-    private float changeValue = 10f;
-    private float direction = -1f;
-    private float minValue = 0f;
-    private float maxValue = 100f;
-
-    public void Start()
+    private void Start()
     {
-        _slider.value = _valueHealth;
-        textHealthValue.text = _valueHealth.ToString();
+        float _tempValue = _controllerHealthValue._valueHealth;
+        _slider.value = _tempValue;
     }
 
-    public void AddHealth()
+    public void AddHealthSlider()
     {
-        StartCoroutine(ChangedSlider(true));
-        ChangedHealth(true);
+        StartCoroutine(ChangedSlider(true));       
     }
 
-    public void TakeHealth()
+    public void TakeHealthSlider()
     {
-        StartCoroutine(ChangedSlider(false));
-        ChangedHealth(false);
-    }
-
-    private void WriteHealthValue(float healthValue)
-    {
-        string valueHealth = healthValue.ToString();
-        textHealthValue.text = valueHealth;
-    }
-
-    private void ChangedHealth(bool IsPositive)
-    {
-        direction = ChooseDirection(IsPositive);
-
-        if ((_valueHealth == minValue && IsPositive == true) || (_valueHealth == maxValue && IsPositive == false) || (_valueHealth > minValue && _valueHealth < maxValue))
-        {
-            _valueHealth += changeValue * direction;
-            WriteHealthValue(_valueHealth);
-        }
+        StartCoroutine(ChangedSlider(false));        
     }
 
     private IEnumerator ChangedSlider(bool IsPositive)
-    {        
-        direction = ChooseDirection(IsPositive);
+    {
+        direction = _controllerHealthValue.ChooseDirection(IsPositive);
 
         float runningTime = 0f;
-        float targetValue = _slider.value + changeValue * direction;
+        float targetValue = _slider.value + _controllerHealthValue.changeValue * direction;
 
         while (runningTime <= _durationChanged)
         {
             runningTime += Time.deltaTime;
-            _slider.value = Mathf.MoveTowards(_slider.value, targetValue, (changeValue / _durationChanged) * Time.deltaTime);
+            _slider.value = Mathf.MoveTowards(_slider.value, targetValue, (_controllerHealthValue.changeValue / _durationChanged) * Time.deltaTime);
             yield return null;
         }
 
         StopCoroutine(ChangedSlider(IsPositive));
-    }
-
-    private float ChooseDirection(bool IsPositive)
-    {
-        if (IsPositive)
-        {
-            return 1f;
-        }
-        else
-        {
-            return -1f;
-        }
-    }
+    } 
 }
